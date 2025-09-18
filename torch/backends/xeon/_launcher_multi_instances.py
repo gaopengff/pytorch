db956db5f1b4453d65ae6cc76f3d6e78435fc836
Gao, Pengfei <pengfei.gao@intel.com>
2025-09-18 02:27:21 -0400

    support non-balance core cpu

--- torch/backends/xeon/_launcher_multi_instances.py
+++ torch/backends/xeon/_launcher_multi_instances.py
@@ -155,8 +155,9 @@ class MultiInstancesLauncher(Launcher):
             if k != "":
                 self.verbose("info", f"env: {k}={v}")
                 environ_local[k] = v
-        # todo: figure out why OMP_NUM_THREADS has been set to 1 already?
-        omp_num_threads = self.check_env("OMP_NUM_THREADS", len(pool))
+        # Overide OMP_NUM_THREADS(default is 1) with len(pool)
+        # omp_num_threads = self.check_env("OMP_NUM_THREADS", len(pool))
+        omp_num_threads = len(pool)
         environ_local["OMP_NUM_THREADS"] = str(omp_num_threads)
         self.verbose("info", f"env: OMP_NUM_THREADS={omp_num_threads}")
 
